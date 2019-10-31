ea6c1471775c7d24d809b0de853ae5f63178169e
Staba1139 <takaaki2232mano@gmail.com>
2019-10-31 11:09:45 +0900

    chapter1-list1-1

---
+++ class-extend-conquest/List_1-1.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+struct Employee {
+    int number;
+    char name[80];
+    int salary;
+
+};
+
+int main(){
+    struct Employee tanaka;
+
+    tanaka.number = 1234;
+    strcpy(tanaka.name, "田中一郎");
+    tanaka.salary = 200000;
+
+    cout << tanaka.number << "\n";
+    cout << tanaka.name << "\n";
+    cout << tanaka.salary << "\n";
+
+    return 0;
+}
