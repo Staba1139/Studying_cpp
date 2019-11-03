1a93bec66020e5cca884052b828f8e9a2a706808
Staba1139 <takaaki2232mano@gmail.com>
2019-11-03 18:51:19 +0900

    3.2 クラスの使い方

---
+++ class-extend-conquest/Chapter3 -Class and objects/List3-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "List3-1_Employee.h"
+
+using namespace std;
+
+void Employee::showData(){
+    cout << number << "\n";
+    cout << name << "\n";
+    cout << salary << "\n";
+}
