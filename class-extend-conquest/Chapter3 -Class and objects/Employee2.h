be8ca88d999a575cad7a85a35f144eb6f478e39e
Staba1139 <takaaki2232mano@gmail.com>
2019-11-09 18:12:17 +0900

    20191109_3章

---
+++ class-extend-conquest/Chapter3 -Class and objects/Employee2.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+    public:
+        int number;
+        char name[80];
+        int salary;
+
+        void showData() {
+            cout << number << "\n";
+            cout << name << "\n";
+            cout << salary << "\n";
+
+        }
+};
+
